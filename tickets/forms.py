d1d5511468058f0823714f14c7c7265c25a070ad
Joseph Peterson <jscpeterson@gmail.com>
2020-01-07 15:05:17 -0700

    Tickets can be assigned and resolved

--- tickets/forms.py
+++ tickets/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from django.forms import Form
 
+from tickets.models import Ticket
+from users.models import HelpDeskUser, GROUP_SUPPORT
+
 
 class NewTicketForm(Form):
 
@@ -10,4 +13,50 @@ class NewTicketForm(Form):
             widget=forms.Textarea,
             label='Please describe your problem.',
             required=True,
+        )
+
+
+class AssignTicketForm(Form):
+
+    def __init__(self, *args, **kwargs):
+        ticket = Ticket.objects.get(id=kwargs.pop('ticket_id'))
+        super(AssignTicketForm, self).__init__(*args, **kwargs)
+
+        # TODO If the user is SUPPORT and not SUPERVISOR, make the only choice themself
+        assignee_queryset = HelpDeskUser.objects.filter(groups__name__in=[GROUP_SUPPORT])
+        self.fields['assignee'] = forms.ModelChoiceField(
+            queryset=assignee_queryset,
+            label='Assign this task to a support agent:',
+            required=True,
+        )
+
+        self.fields['priority'] = forms.ChoiceField(
+            choices=Ticket.PRIORITY_CHOICES,
+            label='Assign a priority level:',
+            required=True,
+        )
+
+        self.fields['category'] = forms.ChoiceField(
+            choices=Ticket.CATEGORY_CHOICES,
+            label='Assign a category:',
+            required=True,
+        )
+
+        self.fields['notes'] = forms.CharField(
+            widget=forms.Textarea,
+            label='Notes:',
+            required=False,
+        )
+
+
+class ResolveTicketForm(Form):
+
+    def __init__(self, *args, **kwargs):
+        ticket = Ticket.objects.get(id=kwargs.pop('ticket_id'))
+        super(ResolveTicketForm, self).__init__(*args, **kwargs)
+
+        self.fields['resolution'] = forms.CharField(
+            widget=forms.Textarea,
+            label='Resolution Notes:',
+            required=False,
         )
--- tickets/views.py
+++ tickets/views.py
@@ -2,16 +2,21 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.utils import timezone
 
 from tickets.models import Ticket
+from users.models import GROUP_SUPERVISOR, GROUP_SUPPORT
 from . import forms
 
 
 @login_required
 def home(request):
-    # return render(request, 'tickets/debug.html', {'name': request.user.first_name})
-    return HttpResponseRedirect(reverse('tickets:view_user_tickets'))
-
+    if request.user.groups.filter(name=GROUP_SUPERVISOR).exists():
+        return HttpResponseRedirect(reverse('tickets:view_unassigned_tickets'))
+    elif request.user.groups.filter(name=GROUP_SUPPORT).exists():
+        return HttpResponseRedirect(reverse('tickets:view_assigned_tickets'))
+    else:
+        return HttpResponseRedirect(reverse('tickets:view_user_tickets'))
 
 @login_required
 def view_user_tickets(request):
@@ -21,6 +26,7 @@ def view_user_tickets(request):
         'open_tickets': Ticket.objects.filter(user=request.user, status=Ticket.OPEN),
         'closed_tickets': Ticket.objects.filter(user=request.user, status=Ticket.CLOSED),
     }
+    # TODO Order tickets by datetimes
 
     return render(request, template, context)
 
@@ -35,41 +41,84 @@ def new_ticket(request, *args, **kwargs):
                 user=request.user,
                 problem_description=form.cleaned_data.get('problem_description')
             )
-        return HttpResponseRedirect(reverse('tickets:view_user_tickets'))
+            return HttpResponseRedirect(reverse('tickets:home'))
     else:
         form = forms.NewTicketForm(*args, **kwargs)
 
     context = {'form': form}
-    return render(request, 'tickets/new_ticket.html', context)
+    return render(request, template, context)
 
 
 @login_required
 def view_unassigned_tickets(request):
     template = 'tickets/view_unassigned_tickets.html'
-    context = {}
+    context = {
+        'first_name': request.user.first_name,
+        'unassigned_tickets': Ticket.objects.filter(status=Ticket.OPEN, assignee=None),
+    }
 
     return render(request, template, context)
 
 
 @login_required
-def assign_ticket(request):
+def assign_ticket(request, *args, **kwargs):
     template = 'tickets/assign_ticket.html'
-    context = {}
+    # TODO Throw 404? if ticket fails to get
+    ticket = Ticket.objects.get(id=kwargs.get('ticket_id'))
 
+    if request.method == 'POST':
+        form = forms.AssignTicketForm(request.POST, *args, **kwargs)
+        if form.is_valid():
+            data = form.cleaned_data
+
+            ticket.assignee = data.get('assignee')
+            ticket.priority = data.get('priority')
+            ticket.category = data.get('category')
+            ticket.notes = data.get('notes')
+            ticket.assignment_date = timezone.now()
+            ticket.save()
+
+            return HttpResponseRedirect(reverse('tickets:home'))
+
+    else:
+        form = forms.AssignTicketForm(*args, **kwargs)
+
+    context = {'form': form}
     return render(request, template, context)
 
 
 @login_required
 def view_assigned_tickets(request):
-    template = 'tickets/view_assigned_ticket.html'
-    context = {}
+    template = 'tickets/view_assigned_tickets.html'
+    context = {
+        'first_name': request.user.first_name,
+        'assigned_tickets': Ticket.objects.filter(assignee=request.user, status=Ticket.OPEN),
+    }
 
     return render(request, template, context)
 
 
 @login_required
-def resolve_ticket(request):
+def resolve_ticket(request, *args, **kwargs):
     template = 'tickets/resolve_ticket.html'
-    context = {}
+    # TODO Throw 404? if ticket fails to get
+    ticket = Ticket.objects.get(id=kwargs.get('ticket_id'))
 
+    # TODO Check Support permission
+    if request.method == 'POST':
+        form = forms.ResolveTicketForm(request.POST, *args, **kwargs)
+        if form.is_valid():
+            data = form.cleaned_data
+
+            ticket.resolution = data.get('resolution')
+            ticket.closed_date = timezone.now()
+            ticket.status = Ticket.CLOSED
+            ticket.save()
+
+            return HttpResponseRedirect(reverse('tickets:home'))
+
+    else:
+        form = forms.ResolveTicketForm(*args, **kwargs)
+
+    context = {'form': form}
     return render(request, template, context)
