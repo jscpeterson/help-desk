11e1e28f945a5f630c9631aa14e6cfdd31c60e79
Joseph Peterson <jscpeterson@gmail.com>
2020-01-07 09:27:22 -0700

    Obscure SECRET_KEY, add .env and .gitignore

--- helpdesk/settings.py
+++ helpdesk/settings.py
@@ -11,16 +11,18 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+dotenv.read_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dsi!xvrc8)dtee7p75z66kxx0@ds)g^drdumwwj3z+4#ibxb5w'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
