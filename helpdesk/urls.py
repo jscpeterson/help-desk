2d417d6bace834d544f4da36234501ecffb79b14
Joseph Peterson <jscpeterson@gmail.com>
2020-01-09 09:08:49 -0700

    Add error pages

--- helpdesk/urls.py
+++ helpdesk/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from . import views
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('admin/', admin.site.urls),
     path('users/', include('django.contrib.auth.urls')),
-    path('tickets/', include('tickets.urls'))
+    path('error/', views.test_exception),
+    path('tickets/', include('tickets.urls')),
 ]
+
+handler403 = 'helpdesk.views.handler403'
+handler404 = 'helpdesk.views.handler404'
+handler500 = 'helpdesk.views.handler500'
