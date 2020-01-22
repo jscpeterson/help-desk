d38c1d51573c839f32d0825916c1c35977359d59
Eric Verner <everner@gsu.edu>
2020-01-22 15:36:09 -0500

    Added dotenv command to manage.py

--- helpdesk/settings/base.py
+++ helpdesk/settings/base.py
@@ -11,11 +11,12 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from os.path import dirname as up
 import dotenv
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = up(up(up(os.path.abspath(__file__))))
 
 dotenv.read_dotenv(os.path.join(BASE_DIR, '.env'))
 
