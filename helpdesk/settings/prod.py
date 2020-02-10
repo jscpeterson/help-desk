c5f9cc349239bacfeaca9a13ca25de52e4da37a6
Joseph Peterson <jscpeterson@gmail.com>
2020-02-10 15:08:05 -0700

    Change prod settings - remove postgres database, debug OFF

--- helpdesk/settings/prod.py
+++ helpdesk/settings/prod.py
@@ -4,22 +4,8 @@ from django_auth_ldap.config import LDAPSearch, NestedActiveDirectoryGroupType
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('POSTGRES_HOST'),
-        'PORT': os.environ.get('POSTGRES_PORT'),
-    }
-}
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
